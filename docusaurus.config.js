968de60b4ac5e80735985f4db0bf539b5bd0bbbd
Shayan-PC <MuhammadAmmarSiddiqui>
2025-12-07 22:52:24 +0500

    main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   url: 'https://your-username.github.io',
   // Set the /<base>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/AI-SpecDriven-Development/',
 
   // GitHub pages deployment config.
   organizationName: 'your-username', // Usually your GitHub org/user name.
